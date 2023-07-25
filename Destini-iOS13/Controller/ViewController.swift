675f575883894922d180d96e69d0e6c701734603
Prasshant Shanmugalingam <stickoking89@icloud.com>
2023-07-26 01:39:35 +0800

    Initial Commit, Added Auto Layout

--- Destini-iOS13/Controller/ViewController.swift
+++ Destini-iOS13/Controller/ViewController.swift
@@ -9,12 +9,23 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var choiceOne: UIButton!
+    @IBOutlet weak var choiceTwo: UIButton!
+    @IBOutlet weak var storyLabel: UILabel!
+    let story0: String = "You see a fork in the road."
+    let choice1: String = "Take a left."
+    let choice2: String = "Take a right."
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        storyLabel.text = story0
+        choiceOne.setTitle(choice1, for: .normal)
+        choiceTwo.setTitle(choice2, for: .normal)
 
     }
 
-
+    @IBAction func choiceMade(_ sender: UIButton) {
+    }
+    
 }
 
